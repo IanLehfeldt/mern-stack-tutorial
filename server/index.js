18eba4fa40a8525f3fde14d45cc167bfc3db3bf0
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-09-10 20:50:42 -0500

    added google passport oauth, passport and keys.js to project

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-    res.send({bye: 'buddy'});
-});
-
 app.listen(PORT);
+
+//Authenticates user by registering  passport through a google strategy
+//Needs a client id and a client secret to pass through
+passport.use(new GoogleStrategy());
