30edccd9012c2eb9c9127ebaf265d5550b103b7c
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-09-11 22:07:12 -0500

    tested authentication with route that shows user profiled id of current session

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,19 @@
 const passport = require('passport');
 
 module.exports = (app) => {
+    //Route handling google authentication
 	app.get(
 		'/auth/google',
 		passport.authenticate('google', {
 			scope: ['profile', 'email']
 		})
-	);
-
-	app.get('/auth/google/callback/', passport.authenticate('google'));
+    );
+    
+    //Once callback from google oauth is received, push to passport
+    app.get('/auth/google/callback/', passport.authenticate('google'));
+    
+    //Testing authentication
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
 };
