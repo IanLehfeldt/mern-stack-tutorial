de48ca417a3701ac3fb320a4537bd8850e58ed28
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-09-11 21:59:29 -0500

    added cookie session to application to handle cookie requests, hooked up passport to use cookie session

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
+//All of the intitial application set-up
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 //Must define user schema before we call it in passport.js
 require('./models/user');
 require('./services/passport');
 
+const app = express();
+
+app.use(
+    cookieSession({
+        // 30 days, 24 hours in a day, 60 minutes in an hour, 60 seconds in a min, 1000 miliseconds in a second.
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+//passport needs to know to use cookie session
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(keys.mongoURI);
 //We exported the entire function from the authRoutes export and call it with the app object.
--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,19 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+//must call serializeUser to set cookies for each session
+//Takes google profile id and turns it into a mongoose model id
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+//Takes mongoose model id and turns it back into google profile id
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
 //Authenticates user by registering  passport through a google strategy
 //Needs a client id and a client secret to pass through
 passport.use(
@@ -22,9 +35,12 @@ passport.use(
 			User.findOne({ googleId: profile.id }).then((existingUser) => {
 				if (existingUser) {
 					//we already have a record with given profile ID
+					done(null, existingUser);
 				} else {
 					//create new user with given profile id
-					new User({ googleId: profile.id }).save();
+					new User({ googleId: profile.id })
+						.save()
+						.then((user) => done(null, user));
 				}
 			});
 		}
